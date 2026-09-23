cdf70e98de667a294cfe7718ed8c3227e2fc82d8
mluevano1 <mluevano29@setoncatholic.org>
2026-09-23 11:37:31 -0700

    Implement basic arithmetic functions in main.py
    Added functions for addition, subtraction, multiplication, and division of two numbers with user input.

---
+++ main.py
@@ -0,0 +1,40 @@
+# Function that adds two numbers
+def add(x,y) :
+    print(x+y)
+
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+
+# Function that subtract two numbers
+def subtract(x,y) :
+    print(x-y)
+
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+
+# Function that multiply two numbers
+def multiply(x,y) :
+    print(x*y)
+
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+
+# Function that divide two numbers
+def divide(x,y) :
+    print(x,y)
+
+
+x = int(input("Enter the first number:"))
+y = int(input("Enter the second number:"))
+
+
+add(9+8)
+subtract(8-4)
+multiply(7*3)
+divide(8,4)
