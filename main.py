9efda686b1135da3ee26ed88bf86b35fe3b9a0ba
mluevano1 <mluevano29@setoncatholic.org>
2026-09-24 21:12:59 +0000

    mk

--- main.py
+++ main.py
@@ -27,14 +27,44 @@ y = int(input("Enter the second number:"))
 
 # Function that divide two numbers
 def divide(x,y) :
-    print(x,y)
+    print(x/y)
 
 
 x = int(input("Enter the first number:"))
 y = int(input("Enter the second number:"))
 
 
-add(9+8)
-subtract(8-4)
-multiply(7*3)
-divide(8,4)
+
+#######################################################
+####### Start of program
+print("Welcome to mah awsome calc app!")
+print("What would you like to do?")
+print("Type (a)dd (s)ubtract (m)ultiply (d)ivde (q)uit")
+
+while True:
+
+    user_choice = input(": ")
+    #print(user_choice)
+    x = int(input("Enter your first name:  "))
+    y = int(input("Enter your second number: "))
+
+
+    if user_choice == 'a' :
+            add(x,y)
+
+    elif user_choice == 's' :
+            sub(x,y)
+
+    elif user_choice == 'm' :
+            mul(x,y)
+
+    elif user_choice == 'd' :
+            div(x,y)1
+
+    elif user_choice == 'q' :
+        break;
+    else:
+        print("Invalid choice")
+
+    if (x,y) == (8,9):
+        print ("89!")
